49cf08c68f44bd79736257a33bcc09e8c6f1aa60
taksh mehta <takshmehta111@gmail.com>
2020-08-06 00:47:38 +0530

    Updates

---
+++ precache-manifest.5e14ca088b7a56649dc973c3fc565085.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1514c0c849771ff9dced377567fe0b",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "91e465ae93f65242b53b",
+    "url": "/shopping-cart/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "74d214893934fd6c20fb",
+    "url": "/shopping-cart/static/css/main.bfe88ada.chunk.css"
+  },
+  {
+    "revision": "91e465ae93f65242b53b",
+    "url": "/shopping-cart/static/js/2.d79376e0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/shopping-cart/static/js/2.d79376e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d214893934fd6c20fb",
+    "url": "/shopping-cart/static/js/main.c664d08a.chunk.js"
+  },
+  {
+    "revision": "0b75c6f893ace446c39a",
+    "url": "/shopping-cart/static/js/runtime-main.3942f4bc.js"
+  }
+]);
